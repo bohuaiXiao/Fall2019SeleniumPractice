618d92fff48fe8c558e10f3ec5e181b11043fce8
bohuaiXiao <tommy19900218@gmail.com>
2020-03-01 15:46:12 -0500

    BasicNavigation   2020年3月1日15:46:03

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -4,7 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //to start selenium
         WebDriverManager.chromedriver ().setup ();
@@ -12,11 +12,67 @@ public class BasicNavigation {
         ChromeDriver driver = new ChromeDriver ();
         // in selenium everything start from webdriver interface
 
-        driver.get ("https://www.google.com");
-        
+        // ChromDriver extends RemoteWebDriver --> implements webDriver
+        driver.get ("https://www.google.com");// to open a website
+        driver.manage ().window ().maximize ();
+
+        Thread.sleep (3000);// for demo, wait 3 seconds
+
+        String title = driver.getTitle ();
+        // method that return page title
+        // return <title> Some tile</title> text
+
+
+        String expectedTitle = "Google";
+        System.out.println ("Searched title is ---->" + title);
+        if (expectedTitle.equalsIgnoreCase (title)){
+            System.out.println ("Test Passed");
+        }else {
+            System.out.println ("Test not Passed");
+        }
+
+        driver.navigate ().to ("https://amazon.com");
+        Thread.sleep (3000);
+
+        if (driver.getTitle ().contains ("Amazon")){
+            System.out.println ("Test Passed");
+        }else {
+            System.out.println ("Test not Passed");
+        }
+        // comeback to google
+        driver.navigate ().back ();
+        Thread.sleep (3000);
+        verifyEquals (driver.getTitle (),"google");
+        //move forward in the browser history
+        driver.navigate ().forward ();
+        verifyEquals (driver.getTitle (),"amazon");
+        System.out.println ("Title = " + driver.getTitle ());
+
+        System.out.println ("URL:  " + driver.getCurrentUrl ());
+
+
+        driver.close ();// to close browser
+
+        driver.navigate ().refresh ();
+        // to reload the page
 
 
 
+
+
+
+
+
+    }
+
+    public static void verifyEquals(String arg1, String arg2){
+
+        if (arg1.toLowerCase ().contains (arg2)){
+
+            System.out.println ("Test Passed");
+        }else {
+            System.out.println ("Test not Passed");
+        }
 
 
     }
