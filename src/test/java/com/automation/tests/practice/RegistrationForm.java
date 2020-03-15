0e389c56368433d560a527b8780daaf71fd55726
bohuaixiao <tommy19900218@gmail.com>
2020-03-15 17:49:12 -0400

    3/14/2020  TestNG             RegistrationForm Practice

---
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -0,0 +1,106 @@
+package com.automation.tests.practice;
+
+import com.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author:
+ * @create:
+ * @date:
+ */
+public class RegistrationForm {
+    private String URL = "http://practice.cybertekschool.com/registration_form";
+    private WebDriver driver ;
+    private By firstNameBy = By.name("firstname");
+    private By lastNameBy = By.name("lastname");
+    private By userNameBy = By.name("username");
+    private By emailBy = By.name("email");
+    private By passwordBy = By.name("password");
+    private By phoneNumberBy = By.cssSelector("#registrationForm > div:nth-child(6) > div > input");
+    private  By maleBy = By.cssSelector("#registrationForm > div:nth-child(7) > div > div:nth-child(1) > label > input[type=radio]");
+    private By DOB = By.name("birthday");
+    private By javaBox = By.id("inlineCheckbox2");
+    private By submitBy = By.cssSelector("button[type='submit']");
+
+    @Test
+    public void test1(){
+
+        driver.findElement(firstNameBy).sendKeys("firstName");
+        driver.findElement(lastNameBy).sendKeys("lastName");
+        driver.findElement(userNameBy).sendKeys("bohuai");
+        driver.findElement(emailBy).sendKeys("xiao@gmail.com");
+        driver.findElement(passwordBy).sendKeys("daaohaoids223w");
+        driver.findElement(phoneNumberBy).sendKeys("571-454-5895");
+        driver.findElement(maleBy).click();
+        driver.findElement(DOB).sendKeys("02/18/2000");
+
+
+        Select select = new Select(driver.findElement(By.name("department")));
+        select.selectByValue("DE");
+
+        Select select1 = new Select(driver.findElement(By.name("job_title")));
+        select.selectByIndex(6);
+
+
+        driver.findElement(javaBox).click();
+        driver.findElement(submitBy).click();
+
+        String expected = "You've successfully completed registration!";
+        String actual = driver.findElement(By.tagName("p")).getText();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyFirstNameLengthTest(){
+        driver.findElement(firstNameBy).sendKeys("a");
+        String excepted = "first name must be more than 2 and less than 64 characters long";
+        BrowserUtils.iWait(3);
+        WebElement warningElement = driver.findElement(By.cssSelector("#registrationForm > div:nth-child(1) > div > small:nth-child(4)"));
+        String text = warningElement.getText();
+        assertTrue(warningElement.isDisplayed());
+    }
+
+    @Test
+    public void verifyAlphabeticLetterOnlyTest(){
+        driver.findElement(firstNameBy).sendKeys("123");
+        BrowserUtils.iWait(3);
+        WebElement warningMessage = driver.findElement(By.cssSelector("#registrationForm > div:nth-child(1) > div > small:nth-child(5)"));
+        assertTrue(warningMessage.isDisplayed());
+
+
+    }
+
+
+
+
+
+
+
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void tearDown(){
+        BrowserUtils.iWait(3);
+        driver.quit();
+    }
+
+
+
+
+}
--- src/test/java/com/automation/tests/vytrack/fleet/VehiclesPageTests.java
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclesPageTests.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import static org.testng.Assert .*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -31,7 +32,7 @@ public class VehiclesPageTests {
     private By subtitleBy = By.className("oro-subtitle");
 
     private By fleetBy = By.cssSelector("#main-menu > ul > li:nth-child(2) > a > span");
-
+    private By pageBy = By.cssSelector("input[type='number']");
     @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().setup();
@@ -42,6 +43,9 @@ public class VehiclesPageTests {
         driver.findElement(usernameBy).sendKeys(username);
         driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
         BrowserUtils.iWait(3);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(fleetBy)).perform();
+        driver.findElement(By.linkText("Vehicles")).click();
     }
     @AfterMethod
     public void tearDrop(){
@@ -51,23 +55,34 @@ public class VehiclesPageTests {
     @Test
     public void verifyPageSubTitle(){
 
-        //driver.findElement(fleetBy).click();
-        //Actions class is used for more advanced browser interactions
-        Actions actions = new Actions(driver);
-        // move to element instead of click
-        Actions actions1 = actions.moveToElement(driver.findElement(fleetBy));
-        actions1.perform();
-        //perform - to execute command
-        //every action should end with perform()
-
-        BrowserUtils.iWait(3);
-        driver.findElement(By.linkText("Vehicles")).click();
+//        //driver.findElement(fleetBy).click();
+//        //Actions class is used for more advanced browser interactions
+//        Actions actions = new Actions(driver);
+//        // move to element instead of click
+//        Actions actions1 = actions.moveToElement(driver.findElement(fleetBy));
+//        actions1.perform();
+//        //perform - to execute command
+//        //every action should end with perform()
+//
+//        BrowserUtils.iWait(3);
+//        driver.findElement(By.linkText("Vehicles")).click();
 
 
         WebElement element = driver.findElement(subtitleBy);
         String subTitle = element.getText();
         System.out.println("subTitle = " + subTitle);
+        String expected = "All Cars";
+        assertEquals(expected,subTitle);
 
+    }
+
+    @Test
+    public void verifyPageNumber(){
+
+
+        String value = driver.findElement(pageBy).getAttribute("value");
+        String  expect = "1";
+        assertEquals(value,expect);
     }
 
 
